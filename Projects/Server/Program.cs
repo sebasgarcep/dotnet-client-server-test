6847bfec2544a86768ed902226c3d06144c09331
Sebastian Garrido <sebasgarcep@gmail.com>
2026-01-30 23:13:38 -0500

    Add swagger docs with swashbuckler

--- Projects/Server/Program.cs
+++ Projects/Server/Program.cs
@@ -1,5 +1,12 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapGet("/test", () => "hello world");
 
